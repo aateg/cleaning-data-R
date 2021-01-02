33fdde37c4b6aa2db929316274d7c5df8d1007b1
Guilherme Fernandes <aateg@live.com>
2021-01-02 17:39:26 -0300

    add activities and remove imports

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 # imports
-library(dplyr)
+#library(dplyr)
 
 # read dataset
 ## train
@@ -11,14 +11,22 @@ test_path <- "./UCI HAR Dataset/test/"
 X_test <- read.table(paste(test_path, 'X_test.txt', sep = ""))
 y_test <- read.table(paste(test_path, 'y_test.txt', sep = ""))
 ## features labels
-features <- read.table('./UCI HAR Dataset/features.txt', col.names = c('index', 'label'))
+features <- read.table(
+    './UCI HAR Dataset/features.txt', 
+    col.names = c('index', 'label'),
+    colClasses = c("NULL", NA)) # ignore first column
 
 # 1. Merges the training and the test sets to create one data set.
 X <- rbind(X_train, X_test)
+y <- rbind(y_train, y_test)
 # add features labels names
 names(X) <- features$label
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-X2 <- X[, grep(pattern="[Mm]ean|[Ss]td", x=colnames(X))]
+X2 <- X[, grep(pattern="mean\\(\\)|std\\(\\)", x=colnames(X))]
 
 # 3. Uses descriptive activity names to name the activities in the data set
+activity <- read.table(
+    './UCI HAR Dataset/activity_labels.txt', 
+    col.names = c('index', 'label'),
+    colClasse = c("NULL", NA))
