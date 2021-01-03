88b73d81197007da6b1ef423196ed659aad00f63
Guilherme Fernandes <aateg@live.com>
2021-01-03 19:52:47 -0300

    fix data path bug

--- run_analysis.R
+++ run_analysis.R
@@ -3,24 +3,24 @@ library(dplyr)
 
 # read dataset
 ## train
-train_path <- "./UCI HAR dataset/train/"
+train_path <- "./UCI HAR Dataset/train/"
 X_train <- read.table(paste(train_path, 'X_train.txt', sep = ""))
 y_train <- read.table(paste(train_path, 'y_train.txt', sep = ""))
 subject_train <- read.table(paste(train_path, 'subject_train.txt', sep = ""))
                             
 ## test
-test_path <- "./UCI HAR dataset/test/"
+test_path <- "./UCI HAR Dataset/test/"
 X_test <- read.table(paste(test_path, 'X_test.txt', sep = ""))
 y_test <- read.table(paste(test_path, 'y_test.txt', sep = ""))
 subject_test <- read.table(paste(test_path, 'subject_test.txt', sep = ""))
 ## features labels
 features <- read.table(
-    './UCI HAR dataset/features.txt', 
+    './UCI HAR Dataset/features.txt', 
     col.names = c('index', 'label'),
     colClasses = c("NULL", NA)) # ignore first column
 ## activity
 activity <- read.table(
-    './UCI HAR dataset/activity_labels.txt', 
+    './UCI HAR Dataset/activity_labels.txt', 
     col.names = c('index', 'label'))
 
 # 1. Merges the training and the test sets to create one data set.
